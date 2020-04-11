94b6485e8e4a88f072e36c16fe0441cbd54166d6
Pierre Lafitte <pierrelafitte@protonmail.com>
2020-04-10 22:39:56 -0400

    adding in options page for static lists

---
+++ src/constants/priorities.js
@@ -0,0 +1,10 @@
+module.exports[
+  ({
+    name: "Low:",
+    message: "Unlocks in ~2 days",
+    priority: 1
+  },
+  { name: "Medium:", message: "Unlocks ~18 hours", priority: 2 },
+  { name: "High:", message: "Unlocks ~6 hours", priority: 3 },
+  { name: "Very High:", message: "Unlocks ~2 hours", priority: 4 })
+];
